0e07da927d83418bbb6be6ed970b9272b8b09899
Ivan Barral <ivanbarral18@gmail.com>
2020-07-04 00:57:46 -0400

    Setting up the react APP

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Columbia Economic Review</h1>
+        <p>Test Page</p>
+      </div>
+    </div>
+  );
+}
