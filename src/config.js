84b3ad35fc45290883af01c8328d8bb95d2e3d22
Ivan Barral <ivanbarral18@gmail.com>
2020-07-28 18:02:27 -0400

    adding netlify

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export default {
+const dev = {
   MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-1",
@@ -15,4 +15,31 @@ export default {
     APP_CLIENT_ID: "5odjpv35ou3m49t63lhn4mgogk",
     IDENTITY_POOL_ID: "us-east-1:6e770ce2-d455-4d5a-903b-0ce67a139a60",
   },
+};
+
+const prod = {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "econmag-bucket",
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://cdjvuib84m.execute-api.us-east-1.amazonaws.com/production/",
+    authorizationType: "NONE",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_E4bYP5bYv",
+    APP_CLIENT_ID: "5odjpv35ou3m49t63lhn4mgogk",
+    IDENTITY_POOL_ID: "us-east-1:6e770ce2-d455-4d5a-903b-0ce67a139a60",
+  },
+};
+
+const config = process.env.REACT_APP_STAGE === "prod" ? prod : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config,
 };
