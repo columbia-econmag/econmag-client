46c267a42435cb2cdfb8d9512d4c3bec7acd0c3c
Ivan Barral <ivanbarral18@gmail.com>
2020-08-28 18:14:42 -0400

    more additions

--- src/containers/Journal.js
+++ src/containers/Journal.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styled from "styled-components";
-import { Spinner, Card, CardGroup, Row, Col } from "react-bootstrap";
+import { Spinner, Card, CardGroup, Row, Col, Collapse } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { onError } from "../libs/errorLib";
 import { LinkContainer } from "react-router-bootstrap";
@@ -50,35 +50,11 @@ const LetterButton = styled.div`
   border-bottom-right-radius: 50px;
 `;
 
-const Letter = styled.div`
-  transition: 0.2s;
-  height: 300px;
-  width: 100%;
-  background-color: red;
-`;
-
-const LetterWrap = styled.div`
-  width: ${width};
-  transition: 0.2s;
-  margin: auto;
-  // &:active {
-  //   width: 100%;
-  // }
-  // &:active ${Letter} {
-  //   height: 500px;
-  // }
-`;
-
 const IssueSection = styled.section`
   padding: 5px 50px;
   // margin-bottom: 50px;
   background-color: #a0bbd3;
   // background-color: rgb(185, 217, 235); pantone blue
-`;
-
-const PageDiv = styled.div`
-  padding: 2%;
-  text-align: center;
 `;
 
 const Header = styled.h2`
@@ -117,6 +93,11 @@ const AuthorText = styled(Card.Subtitle)`
 const LoaderDiv = styled.div`
   height: 3000px !important;
   text-align: center;
+`;
+
+const Letter = styled.div`
+  background-color: #a0bbd3;
+  padding: 0px 50px;
 `;
 
 export default function Category(...props) {
@@ -261,13 +242,117 @@ export default function Category(...props) {
           </InnerSection>
         </IssueSection>
         <div>
-          <LetterWrap>
-            {isClicked ? <Letter>banana</Letter> : null}
-            <LetterButton onClick={() => setClicked(!isClicked)}>
-              <h6 style={{ marginBottom: "0px" }}>Letter From the Editors</h6>
-              <BsChevronDown />
-            </LetterButton>
-          </LetterWrap>
+          <Collapse in={isClicked}>
+            <Letter id="letters">
+              <InnerSection>
+                <div style={{ padding: "30px 40px" }}>
+                  <p>Dear Readers,</p>
+
+                  <p style={{ textIndent: ".5in" }}>
+                    This past semester has been fraught, to say the least. What
+                    started as a tensely watched &ndash; albeit distant &ndash;
+                    news story became an ineluctable and frightening reality for
+                    those of us in Morningside Heights. Though the global
+                    pandemic has undoubtedly had a differential impact on the
+                    economic and physical well-being of individuals in the
+                    Columbia community, we have all had to cope with
+                    unanticipated class cancellations and the indefinite
+                    suspension of in-person campus activities.
+                  </p>
+
+                  <p style={{ textIndent: ".5in" }}>
+                    Compounding individual tragedy is the dire global economic
+                    outlook. The limited data we have at the time of publication
+                    suggests the United States &ndash; like the rest of the
+                    world &ndash; is in the throes of a sudden and dramatic
+                    downturn. As unemployment claims climb to record highs, the
+                    increasingly frequent comparisons made to the Great
+                    Depression become more appropriate by the day.
+                  </p>
+
+                  <p style={{ textIndent: ".5in" }}>
+                    It is under these circumstances that we present to you the
+                    Spring 2020 Issue of the Columbia Economic Review. It is
+                    only through the diligence and flexibility of our staff,
+                    editorial board, and advisor that we were able to publish a
+                    full issue. While the semester has been difficult, this has
+                    not discernibly affected our collective effort. We believe,
+                    unequivocally, that this edition represents the very best in
+                    undergraduate economics research. It demonstrates our
+                    capacity, as students, to make valuable contributions to the
+                    realm of academic inquiry.
+                  </p>
+
+                  <p style={{ textIndent: ".5in" }}>
+                    This issue features rigorous work in a range of topics.
+                    Though there is no thematic connection, they share a common
+                    sensitivity to matters of contemporary importance in
+                    economics, politics, and society. Noah Talerman (University
+                    of Michigan - Ann Arbor) examines the relationship between
+                    income inequality and the adoption of computer technology by
+                    analyzing countries&rsquo; internet penetration rates. Emily
+                    Malpass (Harvard University) analyzes irregularities in the
+                    market for egg donation as a result of adverse selection,
+                    price controls, and consumer preferences. Sophia Cornell
+                    (Columbia University) investigates collusion and other forms
+                    of anti-competitive behavior in the World Cup. Elizaveta
+                    Brover and Rebecca Schleimer (University of Pennsylvania)
+                    examine the literature on childhood disability and its
+                    impact on maternal labor supply. Lastly, we present an
+                    article by Shreya Ganguly (Columbia
+                    University&ndash;Sciences-Po), a member of the
+                    Review&rsquo;s online team, on the alleged right-wing bias
+                    in introductory economics courses.{" "}
+                  </p>
+
+                  <p style={{ textIndent: ".5in" }}>
+                    While we encountered difficulties in the publication
+                    process, we overcame them with the guidance and support of
+                    faculty and student body members. In particular, we would
+                    like to acknowledge the invaluable assistance of Professor
+                    Wouter Vergote, our academic advisor, Lauren Close, the
+                    Program Manage at the Program for Economic Research (PER),
+                    and Dr. Sophia N. Johnson, Assistant Director of PER. Their
+                    continued involvement and encouragement make CER possible.
+                  </p>
+
+                  <p style={{ textIndent: ".5in" }}>
+                    Lastly, we would like to offer a reflection on the coming
+                    decade. Even though significant uncertainties cloud the
+                    economic and social panorama, we know that there will be a
+                    time after this crisis. That time will give all of us
+                    &ndash; including us amateur economists &ndash; the
+                    opportunity to understand the event we are currently living
+                    through. While we may not know when that time will come, CER
+                    will continue to facilitate the probing academic work that
+                    makes sense of a complex world. We expect this crisis to be
+                    the topic of research for years to come, and we hope to
+                    begin that effort whenever sensible.
+                  </p>
+
+                  <p style={{ textIndent: ".5in" }}>
+                    We would like to dedicate this journal in honor of those who
+                    have lost their lives to this pandemic, as well as those
+                    individuals working tirelessly to fight it and prevent
+                    further loss of life.
+                  </p>
+
+                  <p style={{ marginBottom: "2px" }}>Sincerely,</p>
+                  <p style={{ paddingBottom: "5px", marginBottom: "0px" }}>
+                    Ignacio Lopez Gaffney and Sinet Chelagat
+                  </p>
+                </div>
+              </InnerSection>
+            </Letter>
+          </Collapse>
+          <LetterButton
+            onClick={() => setClicked(!isClicked)}
+            aria-controls="letters"
+            aria-expanded={isClicked}
+          >
+            <h6 style={{ marginBottom: "0px" }}>Letter From the Editors</h6>
+            <BsChevronDown />
+          </LetterButton>
         </div>
         <InnerSection>
           {isLoading ? (
---
+++ src/text/LetterEditor.js
@@ -0,0 +1,143 @@
+const text = (
+  <>
+    <p style='margin:0in;font-size:13px;font-family:"Calibri",sans-serif;'>
+      <span style='font-size:16px;font-family:"Times New Roman",serif;'>
+        Dear Readers,
+      </span>
+    </p>
+    <p style='margin:0in;font-size:13px;font-family:"Calibri",sans-serif;'>
+      <br />
+    </p>
+    <p>
+      <span>
+        This past semester has been fraught, to say the least. What started as a
+        tensely watched &ndash; albeit distant &ndash; news story became an
+        ineluctable and frightening reality for those of us in Morningside
+        Heights. Though the global pandemic has undoubtedly had a differential
+        impact on the economic and physical well-being of individuals in the
+        Columbia community, we have all had to cope with unanticipated class
+        cancellations and the indefinite suspension of in-person campus
+        activities.
+      </span>
+    </p>
+    <p>
+      <span>&nbsp;</span>
+    </p>
+    <p>
+      <span>
+        Compounding individual tragedy is the dire global economic outlook. The
+        limited data we have at the time of publication suggests the United
+        States &ndash; like the rest of the world &ndash; is in the throes of a
+        sudden and dramatic downturn. As unemployment claims climb to record
+        highs, the increasingly frequent comparisons made to the Great
+        Depression become more appropriate by the day.
+      </span>
+    </p>
+    <p>
+      <span>&nbsp;</span>
+    </p>
+    <p style='margin:0in;font-size:13px;font-family:"Calibri",sans-serif;text-indent:.5in;line-height:108%;'>
+      <span style='font-size:15px;line-height:108%;font-family:"Times New Roman",serif;'>
+        It is under these circumstances that we present to you the Spring 2020
+        Issue of the Columbia Economic Review. It is only through the diligence
+        and flexibility of our staff, editorial board, and advisor that we were
+        able to publish a full issue. While the semester has been difficult,
+        this has not discernibly affected our collective effort. We believe,
+        unequivocally, that this edition represents the very best in
+        undergraduate economics research. It demonstrates our capacity, as
+        students, to make valuable contributions to the realm of academic
+        inquiry.
+      </span>
+    </p>
+    <p style='margin:0in;font-size:13px;font-family:"Calibri",sans-serif;line-height:.15pt;'>
+      <span>&nbsp;</span>
+    </p>
+    <p style='margin:0in;font-size:13px;font-family:"Calibri",sans-serif;margin-right:7.0pt;line-height:110%;'>
+      <span style='font-size:15px;line-height:110%;font-family:"Times New Roman",serif;'>
+        This issue features rigorous work in a range of topics. Though there is
+        no thematic connection, they share a common sensitivity to matters of
+        contemporary importance in economics, politics, and society. Noah
+        Talerman (University of Michigan - Ann Arbor) examines the relationship
+        between income inequality and the adoption of computer technology by
+        analyzing countries&rsquo; internet penetration rates. Emily Malpass
+        (Harvard University) analyzes irregularities in the market for egg
+        donation as a result of adverse selection, price controls, and consumer
+        preferences. Sophia Cornell (Columbia University) investigates collusion
+        and other forms of anti-competitive behavior in the World Cup. Elizaveta
+        Brover and Rebecca Schleimer (University of Pennsylvania) examine the
+        literature on childhood disability and its impact on maternal labor
+        supply. Lastly, we present an article by Shreya Ganguly (Columbia
+        University&ndash;Sciences-Po), a member of the Review&rsquo;s online
+        team, on the alleged right-wing bias in introductory economics courses.
+      </span>
+    </p>
+    <p>
+      <span style='font-size:15px;line-height:110%;font-family:"Times New Roman",serif;'>
+        <br />
+        &nbsp;
+      </span>
+    </p>
+    <p style='margin:0in;font-size:13px;font-family:"Calibri",sans-serif;line-height:10.0pt;'>
+      <span>&nbsp;</span>
+    </p>
+    <p style='margin:0in;font-size:13px;font-family:"Calibri",sans-serif;line-height:10.0pt;'>
+      <span>&nbsp;</span>
+    </p>
+    <p style='margin:0in;font-size:13px;font-family:"Calibri",sans-serif;line-height:10.0pt;'>
+      <span>&nbsp;</span>
+    </p>
+    <p style='margin:0in;font-size:13px;font-family:"Calibri",sans-serif;line-height:19.1pt;'>
+      <span>&nbsp;</span>
+    </p>
+    <p style='margin:0in;font-size:13px;font-family:"Calibri",sans-serif;margin-right:7.0pt;text-indent:.5in;line-height:105%;'>
+      <span>
+        While we encountered difficulties in the publication process, we
+        overcame them with the guidance and support of faculty and student body
+        members. In particular, we would like to acknowledge the invaluable
+        assistance of Professor Wouter Vergote, our academic advisor, Lauren
+        Close, the Program Manage at the Program for Economic Research (PER),
+        and Dr. Sophia N. Johnson, Assistant Director of PER. Their continued
+        involvement and encouragement make CER possible.
+      </span>
+    </p>
+    <p>
+      <span>&nbsp;</span>
+    </p>
+    <p style='margin:0in;font-size:13px;font-family:"Calibri",sans-serif;text-indent:.5in;line-height:110%;'>
+      <span style='font-size:15px;line-height:110%;font-family:"Times New Roman",serif;'>
+        Lastly, we would like to offer a reflection on the coming decade. Even
+        though significant uncertainties cloud the economic and social panorama,
+        we know that there will be a time after this crisis. That time will give
+        all of us &ndash; including us amateur economists &ndash; the
+        opportunity to understand the event we are currently living through.
+        While we may not know when that time will come, CER will continue to
+        facilitate the probing academic work that makes sense of a complex
+        world. We expect this crisis to be the topic of research for years to
+        come, and we hope to begin that effort whenever sensible.
+      </span>
+    </p>
+    <p style='margin:0in;font-size:13px;font-family:"Calibri",sans-serif;line-height:12.35pt;'>
+      <span>&nbsp;</span>
+    </p>
+    <p style='margin:0in;font-size:13px;font-family:"Calibri",sans-serif;margin-right:10.0pt;text-indent:.5in;line-height:109%;'>
+      <span style='font-size:16px;line-height:109%;font-family:"Times New Roman",serif;'>
+        We would like to dedicate this journal in honor of those who have lost
+        their lives to this pandemic, as well as those individuals working
+        tirelessly to fight it and prevent further loss of life.
+      </span>
+    </p>
+    <p style='margin:0in;font-size:13px;font-family:"Calibri",sans-serif;line-height:12.4pt;'>
+      <span>&nbsp;</span>
+    </p>
+    <p style='margin:0in;font-size:13px;font-family:"Calibri",sans-serif;'>
+      <span style='font-size:16px;font-family:"Times New Roman",serif;'>
+        Sincerely,
+      </span>
+    </p>
+    <p style='margin:0in;font-size:13px;font-family:"Calibri",sans-serif;'>
+      <span style='font-size:16px;font-family:"Times New Roman",serif;'>
+        Ignacio Lopez Gaffney and Sinet Chelagat
+      </span>
+    </p>
+  </>
+);
