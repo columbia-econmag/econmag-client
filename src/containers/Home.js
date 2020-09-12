d86ebe063e316dbf6d2b82c09e9b55272b7c0a53
Ivan Barral <ivanbarral18@gmail.com>
2020-09-12 00:38:19 -0400

    adding more cache fixes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -78,7 +78,7 @@ export default function Home() {
 
         <MobileHeader>Recent Articles</MobileHeader>
         <Suspense fallback={<Spinner animation="border" variant="primary" />}>
-          <CategoriesView query="" />
+          <CategoriesView query="excerpt?limit=3" />
         </Suspense>
         <IssueSection>
           <InnerSection>
