1586b8d0d15775760541e0b658bfa4df1cc01e86
Kent Liu <kent.liu@rschooltoday.com>
2024-01-27 13:04:07 -0500

    Update Announcements for high school essay competition

--- src/containers/Announcements.js
+++ src/containers/Announcements.js
@@ -42,24 +42,56 @@ export default function Announcements() {
 
   return (
     <div className="Articles">
-      <OuterDiv className="margins">
-        <Header id="aboutBlurb" className="uglyJournal">
-          Announcements
-        </Header>
-        <div className="uglyJournal">
-              <p>
-            <b>The Columbia Economic Review is currently taking high school essay submissions until 2/1. Submissions must meet the following requirements and be emailed to{" "}
-                <a href="mailto:econreview@columbia.edu">econreview@columbia.edu</a>:</b>
-            <ol type="1">
-            <li>The content of the paper (not including the bibliography and extra data tables) must not exceed 5 pages. It is the author’s responsibility to trim down their work prior to submitting it.</li>
-            <li>Include the author’s name and university, acknowledgements (if relevant), and image files of all graphics or tables used in the paper. All images should be included in the paper and separate files (e.g., jpeg) should be submitted alongside the paper.</li>
-            <li>Any spreadsheets used should also have relevant data linked.</li>
-            <li>Not have already been published in other journals. By submitting, you give CER the sole right to publish the paper and make any edits that we see fit. Please do not submit to other academic journals.</li>
-            <li>All manuscripts should be submitted in PDF format with 1.5 line spacing. We strongly recommend manuscripts not exceed 40 pages (not including the bibliography and extra data tables). The suggested length includes reference lists, figures, and tables. Submit the .tex file if LaTeX was used). It is the author’s responsibility to condense the thesis prior to submitting the documents.</li>
-            <li>Please use 12-point Times New Roman or similar font. Margins should be 1.5 inches on the top, bottom, and sides.</li>
-            <li>Include an abstract of 100 or fewer words.</li> </ol> </p>
-        </div>
-      </OuterDiv>
+        <OuterDiv className="margins">
+            <Header id="aboutBlurb" className="uglyJournal">
+                Announcements
+            </Header>
+            <div className={`uglyJournal`}>
+                <h3 className={`font-weight-bold text-xl-center`}>CER High School Essay Competition</h3>
+                <p>
+                    <ul>
+                        <li><b>Prompt 1: Microeconomics</b>. Recently, the UK has considered implementing a ban
+                            that would prevent anyone born after 2008 to ever buy products containing tobacco, modeled
+                            after a similar ban in New Zealand. This is an example of governments making a choice for
+                            consumers to curb the effects of
+                            negative externalities. Examine the feasibility and effectiveness of implementing a similar
+                            ban in the US by comparing and contrasting this strategy with another feasible alternative.
+
+                        </li>
+                        <li><b>Prompt 2: Macroeconomics</b>. Market research firm QuestionPro has estimated that
+                            Taylor Swift's tour could
+                            contribute $5 billion to the worldwide economy. Assuming that the tour has led to an
+                            increase in short-run productivity, consider the possible economic
+                            consequences of this "Swiftie fiscal policy" and evaluate the long-run effects of this tour.
+                        </li>
+                        <li>
+                            <b>Prompt 3: Finance</b>. FTX Founder Sam Bankman-Fried has been convicted of seven counts
+                            of fraud and conspiracy by a jury. Cryptocurrency no longer remains on the fringe of
+                            financial theory with its growing relevance. Therefore, consider exploring the historical
+                            context of cryptocurrency and the impact of major currencies like
+                            Bitcoin and Ethereum as well as their benefits and risks in order to address
+                            regulatory challenges governments face.
+                        </li>
+                        <li><b>Prompt 4: Create Your Own Prompt</b>. Feel free to write on any other topic in
+                            the field of economics, and discuss how
+                            your topic of interest connects to real life
+                        </li>
+                    </ul>
+                    <h5 className={`text-decoration-underline font-weight-bold`}>Details</h5>
+                    <p>
+
+                        <b>Email questions to:</b> <a
+                        href="mailto:columbiaeconreview1754@gmail.com">columbiaeconreview1754@gmail.com</a><br/>
+                        <b>Submission via email</b> to <a
+                        href="mailto:columbiaeconreview1754@gmail.com">columbiaeconreview1754@gmail.com</a><br/>
+                        <b>Word count:</b> recommendation is <u>1500 words</u> (3 pages double spaced)<br/>
+                        <b>Font:</b> Times New Roman, 12, standard margins<br/>
+                        <b>Deadline:</b> February 16th, 11:59 EST
+                    </p>
+                </p>
+            </div>
+
+        </OuterDiv>
     </div>
   );
 }
